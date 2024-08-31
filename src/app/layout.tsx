d94c8b757958eb4515b7adea900e3a051414670c
xyzuan <jodyyuan@xyzuan.my.id>
2024-08-31 23:51:30 +0700

    feat(components): Add MagicUI dock

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/ui/sidebar";
+import { BottomNav } from "@/components/ui/bottom-nav";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +20,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} flex justify-center p-8 md:p-24`}>
-        <div className="flex md:gap-6 max-w-6xl">
-          <aside>
-            <Sidebar />
-          </aside>
-          {children}
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex md:gap-6 max-w-6xl">
+            <aside>
+              <Sidebar />
+            </aside>
+            {children}
+          </div>
+          <BottomNav />
+        </ThemeProvider>
       </body>
     </html>
   );
