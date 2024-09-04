b10aad029971d0e8859f749a1c62a0da31972042
xyzuan <jodyyuan@xyzuan.my.id>
2024-09-04 23:38:55 +0700

    feat(seo): Still fixing seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,36 +4,7 @@ import { Manrope } from "next/font/google";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 
-import type { Metadata } from "next";
-import { METADATA } from "@/commons/constants/metadata";
-
 const manrope = Manrope({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : process.env.DOMAIN || ""
-  ),
-  description: METADATA.description,
-  keywords: METADATA.keyword,
-  creator: METADATA.creator,
-  authors: {
-    name: METADATA.creator,
-    url: METADATA.openGraph.url,
-  },
-  openGraph: {
-    images: METADATA.profile,
-    url: METADATA.openGraph.url,
-    siteName: METADATA.openGraph.siteName,
-    locale: METADATA.openGraph.locale,
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-};
 
 export default function RootLayout({
   children,
