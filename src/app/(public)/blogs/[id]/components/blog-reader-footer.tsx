555944827e897381e2c23e73db65191546fef3ff
xyzuan <xyzuannihboss@gmail.com>
2024-09-26 23:00:51 +0700

    fix(blog): Overflow Reaction

--- src/app/(public)/blogs/[id]/components/blog-reader-footer.tsx
+++ src/app/(public)/blogs/[id]/components/blog-reader-footer.tsx
@@ -16,12 +16,12 @@ const BlogReaderFooter = ({
       <Separator />
       <div className="flex flex-col w-full my-3 mt-3">
         <Typography.h4 className="mb-3">Reactions</Typography.h4>
-        <div className="flex gap-4">
+        <div className="flex gap-4 overflow-y-auto scrollbar-hide">
           {reactions.length > 0 ? (
             <>
               {reactions.map((value, idx) => (
                 <div key={idx} className="relative inline-block">
-                  <Avatar className="h-16 w-16">
+                  <Avatar className="h-12 w-12">
                     <AvatarImage
                       src={value.user?.iconUrl}
                       alt={value.user?.name}
@@ -30,7 +30,7 @@ const BlogReaderFooter = ({
                       {value.user?.name?.slice(0, 1)}
                     </AvatarFallback>
                   </Avatar>
-                  <div className="flex justify-center items-center p-2 h-6 w-6 absolute bottom-0 right-0 text-base bg-foreground/55 backdrop-blur-3xl rounded-full">
+                  <div className="flex justify-center items-center p-2 h-5 w-5 absolute bottom-0 right-0 text-xs bg-foreground/55 backdrop-blur-3xl rounded-full">
                     {getEmoteLabel(value.type.toUpperCase())}
                   </div>
                 </div>
